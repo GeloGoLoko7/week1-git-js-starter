dfd9c39015de812f1154664544f529e78af0121c
GeloGoLoko7 <gelomacchiato777@gmail.com>
2026-07-01 14:45:29 +0800

    Math Functions

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ console.log('=== Week 1: Git & JavaScript Starter ===\n');
 console.log('--- Math Utilities ---');
 
 // TODO: Uncomment and test these once you implement the math functions
-// console.log('5 + 3 =', math.add(5, 3));
+console.log('5 + 3 =', math.add(5, 3));
 // console.log('10 - 4 =', math.subtract(10, 4));
 // console.log('6 * 7 =', math.multiply(6, 7));
 // console.log('20 / 4 =', math.divide(20, 4));
